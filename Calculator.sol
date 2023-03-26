54f7524973e34de07e478831a3af98d69ce600ee
Jitumani Das <76507466+Jitumani073@users.noreply.github.com>
2023-03-26 20:14:07 +0530

    Update Calculator.sol

--- Calculator.sol
+++ Calculator.sol
@@ -3,16 +3,19 @@ pragma solidity 0.8.18;
 
 contract Calculator{
 
-    function add(uint _a, uint _b)external pure returns(uint){
+    function add(uint _a, uint _b) external pure returns(uint){
         return _a + _b;
     }
-    function sub(int _a, int _b)external pure returns(int){
+    
+    function sub(int _a, int _b) external pure returns(int){
         return _a - _b;
     }
-    function mult(uint _a, uint _b)external pure returns(uint){
+    
+    function mult(uint _a, uint _b) external pure returns(uint){
         return _a * _b;
     }
-    function div(uint _a, uint _b)external pure returns(uint){
+    
+    function div(uint _a, uint _b) external pure returns(uint){
         return _a / _b;
     }
     
