1c4dab84b6e92084842fdacdbffd50dd12fcc89c
Jitumani Das <76507466+Jitumani073@users.noreply.github.com>
2023-03-26 16:57:23 +0530

    Create Calculator.sol

---
+++ Calculator.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Calculator{
+
+    function add(uint _a, uint _b)external pure returns(uint){
+        return _a + _b;
+    }
+    function sub(int _a, int _b)external pure returns(int){
+        return _a - _b;
+    }
+    function mult(uint _a, uint _b)external pure returns(uint){
+        return _a * _b;
+    }
+    function div(uint _a, uint _b)external pure returns(uint){
+        return _a / _b;
+    }
+    
+}
